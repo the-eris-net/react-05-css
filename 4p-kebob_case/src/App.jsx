8064ae21e31163652479c5ed35dc3799a8085d31
the-eris-net <the.eris.net@gmail.com>
2025-09-26 05:22:12 +0900

    feat:add rule

--- 4p-kebob_case/src/App.jsx
+++ 4p-kebob_case/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
+import './App.css';
 
 function Card({ title, children }) {
   return (
-    <div
-      style={{ border: '1px solid gray', 
-        borderRadius: '8px', 
-        padding: '10px' }}
-    >
-      <h2>{title}</h2>
-      <div>{children}</div>
+    <div className='card' >
+      <h2 className='card-title'>{title}</h2>
+      <div className='card-body'>{children}</div>
     </div>
   );
 }
@@ -17,12 +13,12 @@ function App() {
   return (
     <>
       <Card title="서류 제출 마감 기한">
-        <input type="date" defaultValue="2025-09-15" />
+        <div>2025-09-15</div>
       </Card>
       <Card title="오늘 공부 체크리스트">
-        <ul>
-          <li>스터디 준비</li>
-          <li>예제 코드 작성</li>
+        <ul className='list'>
+          <li className='list-item'>스터디 준비</li>
+          <li className='list-item-highlight'>예제 코드 작성</li>
         </ul>
       </Card>
     </>
